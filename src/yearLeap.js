741772801a4d4e9a42b3ef6cd4d79c3972be79a8
Andres Camilo Rodriguez Espitia <arodrigueze@pslcol.com.co>
2018-01-22 09:34:23 -0500

    test 2 and 3

--- src/yearLeap.js
+++ src/yearLeap.js
@@ -1,6 +1,10 @@
 function isLeapYear(year) {
-    var isLeap = year%4 === 0;
-    return isLeap;
+
+    if(year % 400 === 0) return true;
+    else if (year % 100 === 0) return false;
+    else if (year % 4 === 0) return true;
+    else return false;
+
 }
 
 module.exports = isLeapYear;
